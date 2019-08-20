851951e7a2b20bca47e784643e5ef90787571d75
Tomasz <tomasz.pieniezny@vp.pl>
2019-08-20 22:03:53 +0200

    Zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        Calculator calculator = new Calculator(5, 2);
+
+        int resultAdd = calculator.add(5, 2);
+        int resultSubstract = calculator.subtract(5, 2);
+
+
+        if (resultAdd == 7 && resultSubstract == 3) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        System.out.println("Test - drugi test jednostkowy:");
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+}
