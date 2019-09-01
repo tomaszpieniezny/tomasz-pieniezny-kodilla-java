e94d6ee4e64551336decc235c720430e6f446bba
Tomasz <tomasz.pieniezny@vp.pl>
2019-09-01 22:11:55 +0200

    Zadanie 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,18 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -26,5 +36,85 @@ public class StreamMain {
         poemBeautifier.beautify("ABC", text -> text.toLowerCase() + " jakis tekst");
         poemBeautifier.beautify("Cos tam napisane", text -> text.substring(4,8));
         poemBeautifier.beautify("eemmeemmm", text -> text.replace("m","S"));
+
+        System.out.println("\nUsing Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+        People.getList().stream()
+                .forEach(System.out::println);
+
+        System.out.println("\nLambdas");
+        People.getList().stream()
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println);
+
+        System.out.println("\nMethod references");
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .forEach(s -> System.out.println(s));
+
+        System.out.println("\nMore than 11 characters");
+        People.getList().stream()
+                .filter(s -> s.length() > 11)
+                .forEach(System.out::println);
+
+        System.out.println("\nBook");
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+        System.out.println("\ngetList");
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        System.out.println("\nCollectors toList");
+        BookDirectory theBookDirectory2 = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory2.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+
+        System.out.println("\nCollectors toMap");
+        BookDirectory theBookDirectory3 = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory3.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+        System.out.println("\nCollectors joining");
+        BookDirectory theBookDirectory4 = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory4.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
+
+        System.out.println("\nZadanie 7.3");
+        Forum forum = new Forum();
+
+        Map<Integer, ForumUser> theResultMapOfForumUser = forum.getUserList().stream()
+                .filter(ForumUser -> ForumUser.getPostCount() > 0)
+                .filter(ForumUser -> ForumUser.getBirthdate().getYear() < 1999)
+                .filter(ForumUser -> ForumUser.getSex() == 'M')
+                .collect(Collectors.toMap(ForumUser::getID,ForumUser -> ForumUser));
+
+        System.out.println("# elements: " + theResultMapOfForumUser.size());
+        theResultMapOfForumUser.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -0,0 +1,51 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+
+public final class ForumUser {
+    private final int ID;
+    private final String userName;
+    private final char sex;
+    private final LocalDate birthDate;
+    private final int postCount;
+
+    public ForumUser(final int ID, final String userName, final char sex, final int yearOfBirth,
+                     final int monthOfBirth, final int dayOfBirth, final int postCount) {
+        this.ID = ID;
+        this.userName = userName;
+        this.sex = sex;
+        this.birthDate = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);;
+        this.postCount = postCount;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthDate;
+    }
+
+    public int getPostCount() {
+        return postCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ForumUser{" +
+                "ID=" + ID +
+                ", userName='" + userName + '\'' +
+                ", sex=" + sex +
+                ", birthDate=" + birthDate +
+                ", postCount=" + postCount +
+                '}';
+    }
+}
