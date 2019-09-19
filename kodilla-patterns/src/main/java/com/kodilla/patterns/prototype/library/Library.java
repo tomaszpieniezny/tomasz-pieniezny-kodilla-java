8866d278b2ec1358302fc0c70c16d610c72c10fc
Tomasz <tomasz.pieniezny@vp.pl>
2019-09-19 20:17:50 +0200

    Zadanie 10.2

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -1,4 +1,47 @@
 package com.kodilla.patterns.prototype.library;
 
-public class Library {
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype {
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        String s = "   List [" + name + "]";
+        for (Book task : books) {
+            s = s + "\n" + task.toString();
+        }
+        return s;
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for(Book theList : books) {
+            Book clonedList = new Book(theList.getTitle(), theList.getAuthor(), theList.getPublicationDate());
+            clonedLibrary.getBooks().add(clonedList);
+        }
+        return clonedLibrary;
+    }
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library)super.clone();
+    }
 }
--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Prototype.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Prototype.java
@@ -1,4 +1,8 @@
 package com.kodilla.patterns.prototype.library;
 
-public class Prototype {
+public class Prototype<T> implements Cloneable {
+    @Override
+    public T clone() throws CloneNotSupportedException {
+        return (T)super.clone();
+    }
 }
